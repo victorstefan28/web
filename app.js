4b5f8c752d12625bf37f60e88b83cca011b9ba01
victorstefan28 <116149427+victorstefan28@users.noreply.github.com>
2023-05-31 00:42:48 +0300

    proiect final

--- app.js
+++ app.js
@@ -21,11 +21,30 @@ app.get('/produse', (req, res) => {
 app.get('/contact', (req, res) => {
   res.sendFile(__dirname + "/contact.html")
 })
+app.post('/contact', (req, res) =>
+{
+  console.log("posttt");
+  console.log(req.body);
+  path = __dirname + "/tickete.txt";
+  fs.appendFile('tickete.txt', 'Nume: ' + req.body.name + ' Mesaj: ' + req.body.message + ' ' + "E-Mail: " + req.body.email + "\n", 'utf-8', (err) =>
+  {
+    if(err)
+    {
+      console.log(err);
+      res.send("Eroare: Mesajul nu a fost trimis!")
+    }
+  })
+  res.send("Mesajul a fost trimis!");
+
+})
 
 app.get('/despre', (req, res) => {
   res.sendFile(__dirname + "/despre.html")
 })
-
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', (req, res) => {
+  res.status(404).sendFile(__dirname + "/404.html")
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function()
 	load();
 
 }
-
+var is_admin = false;
 function load()
 {
 	var darkModeToggle = document.querySelector('#dark-mode-toggle');
@@ -16,6 +16,80 @@ function load()
 			element.classList.toggle('dark-mode');
 		});
 	}
+	var footer = document.querySelector('footer p');
+	var data = new Date();
+	var p_d = document.createElement("p");
+	//p_d.style.display = 'inline';
+	p_d.innerHTML = data.getFullYear() + ' ' + data.getDate() + '/' + data.getMonth();
+	footer.appendChild(p_d);
+
+	/// adaugari in cos
+
+	var colectie_prod = document.querySelectorAll(".card-body > .btn");
+	console.log(colectie_prod[0]);
+	var cart = document.querySelector(".cart-content");
+	console.log(cart);
+	for(var buton of colectie_prod)
+	{
+		buton.onclick = function()
+		{
+			var b = event.target;
+			var div_p = b.parentElement;
+			var titlu = div_p.querySelector(".card-title").innerHTML;
+			console.log(titlu);
+			var pret = (div_p.querySelector(".card-price").innerHTML);
+			var pret_str = pret.split(" ");
+			var pret_int = parseInt(pret_str[0]);
+			console.log(pret_int);
+			var add_crt = document.createElement("li");
+			add_crt.innerHTML = titlu + "-" + pret;
+			var ul_cart = cart.querySelector("ul");
+
+			ul_cart.appendChild(add_crt);
+			var total = document.querySelector(".cart-total");
+			add_crt.onclick = function()
+			{
+
+				var pret_cos = parseInt((event.target.innerHTML.split("-")[1]).split(" ")[0]);
+				total.innerHTML = (parseInt(total.innerHTML.split(" ")[0]) - pret_cos) + "Lei";
+				event.target.remove();
+				event.stopPropagation(); 
+			}
+			
+			total.innerHTML = (parseInt(total.innerHTML.split(" ")[0]) + pret_int) + "Lei";
+		}
+	}
+	/// raspuns node
+	var formular = document.getElementById("contact-form");
+	var rasp_contact = document.querySelector(".raspuns");
+	if(formular != null)
+	{
+		formular.onsubmit = async function()
+		{
+			event.preventDefault();
+			var data = new FormData(this);
+			 var jsonData = {};
+			 data.forEach(function(value, key) {
+	    			jsonData[key] = value;
+	  			});
+			var rasp = await fetch("/contact", {
+				method: "post",
+				headers: {
+	      			"Content-Type": "application/json"
+	    		},
+				body: JSON.stringify(jsonData)
+			})
+			var msg = await rasp.text();
+			rasp_contact.innerHTML = msg;
+			
+		}
+	}
+	var help_red = document.querySelector(".dot");
+	if(help_red != null){help_red.onclick = function()
+		{
+			console.log("help!!");
+			window.location.href = "/contact"
+		}}
 }
 
 function toggleDarkMode()
